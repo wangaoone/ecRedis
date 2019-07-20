04e88ac8395aa8bb85d9dc4c16a0953678f9e06a
Ubuntu <ubuntu@ip-172-31-83-204.ec2.internal>
2019-07-20 23:43:30 +0000

    consistent hashing checked in; but seems that the consistent lib is slow...

--- client.go
+++ client.go
@@ -2,26 +2,39 @@ package ecRedis
 
 import (
 	"bytes"
+	"github.com/buraksezer/consistent"
 	"github.com/klauspost/reedsolomon"
+	"github.com/seiflotfy/cuckoofilter"
 	"github.com/wangaoone/redeo/resp"
 	"net"
 )
 
+type Conn struct {
+	conn net.Conn
+	W    *resp.RequestWriter
+	R    resp.ResponseReader
+}
+
 type Client struct {
-	ConnArr  []net.Conn
-	W        []*resp.RequestWriter
-	R        []resp.ResponseReader
-	ChunkArr [][]byte
-	EC       reedsolomon.Encoder
-	Rec      bytes.Buffer
+	//ConnArr  []net.Conn
+	//W        []*resp.RequestWriter
+	//R        []resp.ResponseReader
+	Conns        map[string][]Conn
+	ChunkArr     [][]byte
+	EC           reedsolomon.Encoder
+	Rec          bytes.Buffer
+	MappingTable map[string]*cuckoo.Filter
+	Ring         *consistent.Consistent
 }
 
 func NewClient(dataShards int, parityShards int, ecMaxGoroutine int) Client {
 	return Client{
-		ConnArr:  make([]net.Conn, dataShards+parityShards),
-		W:        make([]*resp.RequestWriter, dataShards+parityShards),
-		R:        make([]resp.ResponseReader, dataShards+parityShards),
-		ChunkArr: make([][]byte, dataShards+parityShards),
-		EC:       NewEncoder(dataShards, parityShards, ecMaxGoroutine),
+		//ConnArr:  make([]net.Conn, dataShards+parityShards),
+		//W:        make([]*resp.RequestWriter, dataShards+parityShards),
+		//R:        make([]resp.ResponseReader, dataShards+parityShards),
+		Conns:        make(map[string][]Conn),
+		ChunkArr:     make([][]byte, dataShards+parityShards),
+		EC:           NewEncoder(dataShards, parityShards, ecMaxGoroutine),
+		MappingTable: make(map[string]*cuckoo.Filter),
 	}
 }
